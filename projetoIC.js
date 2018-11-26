e58547875a7584f93c60d017950a8c7beabb1948
mariaaugustamb <43381832+mariaaugustamb@users.noreply.github.com>
2018-11-26 19:50:21 -0300

    Add files via upload

--- projetoIC.js
+++ projetoIC.js
@@ -4,6 +4,7 @@
  *
  * Written by Fernando Castor in November/2017.
  */
+//CHECKED
 exports.solve = function(fileName) {
     let formula = readFormula(fileName)
     let result = doSolve(formula.clauses, formula.variables)
@@ -11,22 +12,19 @@ exports.solve = function(fileName) {
   }
   
   // Receives the current assignment and produces the next one
+  //CHECKED
     var cont = 0
   function nextAssignment(currentAssignment) {
     // implement here the code to produce the next assignment based on currentAssignment.
     cont = cont +1
-        var  aux = ""
         var aux2 = 0
-        for (var i = 0; i<currentAssignment.length; i++){
-             aux = aux + assignment[i]
-        }
-        for (var j = 0; j<currentAssignment.length; j++){
-            if (aux.charAt(j) == 1){
-            aux2 = aux2 + Math.pow(2,aux.length-1-j)
+        for(let i = 0 ; i < currentAssignment ; i++){
+            if(assignment[i] == 1){
+                aux2 = aux2 + Math.pow(2, aux.length-1-i);
             }
         }
         
-        aux2 = aux2 + 1;
+        aux2 = aux2 + 1
         
         aux3 = aux2.toString(2)
 
@@ -38,36 +36,32 @@ exports.solve = function(fileName) {
   }
 
 
-    cont2 = 0;
-    var roda = false
-  function doSolve(clauses, assignment) {
+cont2 = 0;
+var roda = false
+function doSolve(clauses, assignment) {
     let isSat = false
     while ((!isSat) && cont <= Math.pow(2,assignment.length)) {
       // does this assignment satisfy the formula? If so, make isSat true.
         for ( var i = 0; i<clauses; i++){
             roda = false
-            for ( var j = 0; j<separando.length && !roda; j++){
+            for ( var j = 0; j<separando[i].length && !roda; j++){
                 if (separando[i][j]<0){
-                    aux4 = separando[i][j]-(2*separando[i][j])
-
-                    if (assignment[aux4] == 0){
+                    if (assignment[separando[i][j]] == 0){
                         cont2=cont2+1;
                         roda = true
                     }
-                }else if (separando[i][j]>=0){
+                }else if (separando[i][j]>0){
                     if (assignment[separando[i][j]] == 1){
                         cont2 = cont2 + 1;
                        roda = true
                     }
-                } if (cont2 == clauses){
+                }
+                if (cont2 == clauses){
                     isSat = true
                 }
             }
-            // if not, get the next assignment and try again.
-            if (cont < Math.pow(2,assignment.length)){
-             assignment = nextAssignment(assignment)
-            }
         }
+        assignment = nextAssignment(assignment)
     }
     let result = {'isSat': isSat, satisfyingAssignment: null}
     if (isSat) {
@@ -75,16 +69,18 @@ exports.solve = function(fileName) {
     }
     return result
   }
-   
+
+  //CHECKED
   function readFormula(fileName) {
     // To read the file, it is possible to use the 'fs' module.
     // Use function readFileSync and not readFile.
     // First read the lines of text of the file and only afterward use the auxiliary functions.
-    let text  = fs.readFileSnyc(fileName, 'utf8')  //  an array containing lines of text extracted from the file.
-    var arrauAux = text.split("\n")
+    const fs = require('fs')
+    let text  = fs.readFileSync(fileName, 'utf8')  //  an array containing lines of text extracted from the file.
+    var arrayAux = text.split("\n")
   
-    let clauses = readClauses(text)
-    let variables = readVariables(clauses)
+    let clauses = readClauses(arrayAux)
+    let variables = readVariables()
    
     // In the following line, text is passed as an argument so that the function
     // is able to extract the problem specification.
@@ -97,41 +93,60 @@ exports.solve = function(fileName) {
     }
     return result
   }
-  
-      var cont2=0
-  function readClauses(text){
-      for (var i = 0; i<text.length; i++ ){
-          if (text[i][0]== "p"){
-              var cv = text[i].split(" ");
-              var vars = cv[2]
-              var clauses = cv[3]
-          }else if (text[i][0] == "cnf"){
-              var tudo
-              //var tamanho = text[i].length;
-              for (var j = 0; j<text[i].length; j++){
-                  //for (var d = 0; d<text[i][cont].length; d++){
-                      tudo[cont2] = text[i][j]
-                      cont2++
-                  }
-              }else {
 
-              }
-          }
-          var separando = tudo.split("0") // separando as clausulas
-      }
-      function readVariables(clauses) {
+//CHECKED
+var cont3=0
+
+function readClauses(text){
+    let aux5 = ""
+    var tudo = ""
+    for (var i = 0; i<text.length; i++ ){
+        if (text[i][0]== "p" || text[i][1] == "cnf"){
+            var cv = text[i].split(" ");
+            var vars = cv[2]
+            var clauses = cv[3]
+        }else if (text[i][0] != 'c'){
+            for (var j = 0; j<text[i].length; j++){
+                if(text[i][j] !== 0){
+                    aux5 = aux5 + text[i][j] + " "
+                }else{
+                    separando[cont3] = aux5.split(" ")
+                    cont3++
+                }
+            }
+            for ( let i = 0 ; i<separando.length ; i++){
+                let compara
+                if ( i = 0){
+                    compara = separando[i].length
+                }else{
+                    aux6 = separando[i].length
+                    if (compara<aux6){
+                        compara = aux6
+                        vars = compara
+                    }
+                }
+                
+            }
+        }
+    }
+    var separando = tudo.split("0") // separando as clausulas
+}
+
+//CHECKED
+function readVariables() {
           for (var i = 0; i<vars; i++){
                variables[i] = 0 
-          }
-          assignment = variables
-      }
-  
-      function checkProblemSpecifications() {
-          if (vars == variables.length && separando.length == clauses){
-              return true
-          }else {
+        }
+          var assignment = variables
+    }
+
+//CHECKED
+function checkProblemSpecifications() {
+    if (separando.length == clauses){
+        return true
+    }else {
               console.log("Entrada Inválida")
               return false
-          }
-      }
+        }
+    }
   
